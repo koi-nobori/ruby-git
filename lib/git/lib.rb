3e42fb3fc6503d1978d7dd540ecc2815e02f0878
Ppsshh <psh@fruitcode.net>
2017-08-13 23:27:46 +0300

    Adds '--all' option to 'git log'
    Usage: g.log.all[0]
      Get latest commit among all branches (?)
    
    --all
      Pretend as if all the refs in refs/, along with HEAD, are listed
      on the command line as <commit>

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -987,6 +987,7 @@ module Git
       arr_opts << "--until=#{opts[:until]}" if opts[:until].is_a? String
       arr_opts << "--grep=#{opts[:grep]}" if opts[:grep].is_a? String
       arr_opts << "--author=#{opts[:author]}" if opts[:author].is_a? String
+      arr_opts << "--all" if opts[:all]
       arr_opts << "#{opts[:between][0].to_s}..#{opts[:between][1].to_s}" if (opts[:between] && opts[:between].size == 2)
 
       arr_opts
